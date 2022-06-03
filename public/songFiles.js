fed6ffe3ce92b02513bf6c6a5d226aae22c05617
MattYip <100390881+MattYip@users.noreply.github.com>
2022-06-03 12:38:28 +1000

    Create songFiles.js

---
+++ public/songFiles.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'Lofi Study Beats',
+        path: 'music/Lofi.mp3',
+        artist: '\'Stomp\'s Playlist\'',
+        cover: 'coverart/Lofi.jpg'
+    },
+    {
+        name: 'Calming Rain Sounds',
+        path: 'music/Rain.mp3',
+        artist: 'Giorgio Magarò',
+        cover: 'coverart/Rain.jpg'
+    },
+    {
+        name: 'Cafe Sounds',
+        path: 'music/Cafe.mp3',
+        artist: 'Nomadic Ambience',
+        cover: 'coverart/Cafe.jpg'
+    },
+]
